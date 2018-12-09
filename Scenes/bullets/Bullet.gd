1d42610ed1f346f158f353705d36e579a82045e2
ItsADogPen <44516836+ItsADogPen@users.noreply.github.com>
2018-12-10 02:41:30 +0800

    Add files via upload
    Check the previous commit.

---
+++ Scenes/bullets/Bullet.gd
@@ -0,0 +1,14 @@
+extends RigidBody2D
+
+export (int) var bullet_speed = 20
+
+# Called when the node enters the scene tree for the first time.
+#func _ready():
+#	pass
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#
+#
+#
+#	position.x += bullet_speed
